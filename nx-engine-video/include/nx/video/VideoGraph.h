aee6acab25e6534a1a4069c78d8005e91dac43ed
alenjiji <alen.jiji.70@gmail.com>
2026-01-27 14:00:31 +0530

    fix: remove constexpr from VideoGraph vector comparisons

--- nx-engine-video/include/nx/video/VideoGraph.h
+++ nx-engine-video/include/nx/video/VideoGraph.h
@@ -8,14 +8,14 @@ struct VideoNode final {
     NodeId id;
     VideoNodeType type;
 
-    constexpr bool operator==(const VideoNode&) const = default;
+    bool operator==(const VideoNode&) const = default;
 };
 
 struct VideoGraph final {
     GraphId id;
     std::vector<VideoNode> nodes;
 
-    constexpr bool operator==(const VideoGraph&) const = default;
+    bool operator==(const VideoGraph&) const = default;
 };
 
 }
