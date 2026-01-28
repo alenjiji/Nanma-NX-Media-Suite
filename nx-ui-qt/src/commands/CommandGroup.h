d6c207aa54e1b8d19584b1a6dcd23eaf8fdc828f
alenjiji <alen.jiji.70@gmail.com>
2026-01-28 20:59:31 +0530

    Phase 16.4.1: add canonical CommandGroup enum for CLI presentation

---
+++ nx-ui-qt/src/commands/CommandGroup.h
@@ -0,0 +1,12 @@
+#pragma once
+
+enum class CommandGroup {
+    CoreOperations,
+    AudioProcessing,
+    VideoProcessing,
+    MetadataAndContainer,
+    BatchAndAutomation,
+    InspectionAndValidation,
+    SystemAndInfrastructure,
+    HelpAndInformation
+};
